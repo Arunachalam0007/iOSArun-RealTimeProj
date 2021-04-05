164cacebcc4204427f1bcde0a1bd0ae5a3f57c14
Arunachalam0007 <arunsha0007@gmail.com>
2021-04-05 18:51:19 +0530

    GoogleMap-Routes: Created Func of GMSMarker and setRoute

--- Sha-GoogleMap-Routes/Sha-GoogleMap-Routes/GoogleMapViewController.swift
+++ Sha-GoogleMap-Routes/Sha-GoogleMap-Routes/GoogleMapViewController.swift
@@ -7,20 +7,57 @@
 
 import UIKit
 import GoogleMaps
+import Alamofire
+import SwiftyJSON
 
 class GoogleMapViewController: UIViewController {
     
     var sourceLocation:String = ""
     var destinationLocation:String = ""
     var apiKey:String = ""
+    var sourceLatitude:Double = 0.0
+    var sourceLongitude:Double = 0.0
+    var destinationLatitude:Double = 0.0
+    var destinationLongitude:Double = 0.0
     
     @IBOutlet var googleMapView: GMSMapView!
+    
     func getMapAPIURL() -> String {
         let mapURL = "https://maps.googleapis.com/maps/api/directions/json?origin=\(sourceLocation)&destination=\(destinationLocation)&key=\(apiKey)"
         return mapURL
     }
     
+    func setRoute(mapAPIURL:String) {
+        
+        AF.request(mapAPIURL).responseJSON { (response) in
+            guard let responseData = response.data else {
+                return
+            }
+            print("responData: ",responseData)
+            do{
+                let responseJSONData = try JSON(data: responseData)
+                print("responseJSONData: ",responseJSONData)
+            }
+            catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
     
+    func setGSMMarker() {
+        
+        let sourceMaker = GMSMarker()
+        let destinationMaker = GMSMarker()
+        
+        sourceMaker.position = CLLocationCoordinate2D(latitude: Double(sourceLatitude), longitude: sourceLongitude)
+        sourceMaker.title = sourceLocation
+        sourceMaker.map = googleMapView
+        
+        destinationMaker.position = CLLocationCoordinate2D(latitude: Double(destinationLatitude), longitude: destinationLongitude)
+        destinationMaker.title = destinationLocation
+        destinationMaker.map = googleMapView
+    }
     
 
     override func viewDidLoad() {
