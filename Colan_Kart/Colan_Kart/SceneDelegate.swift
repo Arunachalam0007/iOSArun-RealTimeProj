5a7f7eb10d2fbb879298f2b8a24b2d98cfbf340a
Arunachalam0007 <arunsha0007@gmail.com>
2021-05-26 19:26:49 +0530

    Colan_Kart: Added Core-Data Model

--- Colan_Kart/Colan_Kart/SceneDelegate.swift
+++ Colan_Kart/Colan_Kart/SceneDelegate.swift
@@ -16,30 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
-        
-        
         guard let scene = (scene as? UIWindowScene) else { return }
-        
-//        window = UIWindow(windowScene: scene)
-//        let storyboard = UIStoryboard(name: "Profile", bundle: nil)
-//        let vc = storyboard.instantiateViewController(identifier: "ProfileVC") as! ProfileViewController
-//        window?.rootViewController = UINavigationController(rootViewController: vc)
-//        window?.makeKeyAndVisible()
-        
-//
-//        window = UIWindow(windowScene: scene)
-//        let storyboard = UIStoryboard(name: "KartStoryboard", bundle: nil)
-//        let vc = storyboard.instantiateViewController(identifier: "KartVC") as! KartTableViewController
-//        window?.rootViewController = UINavigationController(rootViewController: vc)
-//        window?.makeKeyAndVisible()
-        
-        
         window = UIWindow(windowScene: scene)
         let mainTabVC = MainTabController()
         window?.rootViewController = mainTabVC
         window?.makeKeyAndVisible()
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -68,8 +49,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
 }
-
